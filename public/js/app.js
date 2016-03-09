11a79fec4817474bfb9296fd8c4db8ee5c59968f
JesperHonders <jesper.honders@hva.nl>
2016-03-09 11:48:29 +0100

    Added Spritesheet instead of using icon png

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,22 @@
-$(document).ready(function(){
-    $('.product').first().addClass('product-active');
-    $(".product-indicator[data-uuid='"+$('.product').first().data('uuid')+"']").addClass('product-indicator-active');
+/*global document*/
+    
+var product = document.querySelector('.product'),
+    indicator = document.querySelector('.product-indicator[data-uuid="' + product.attributes[1].value + '"]'),
+    indicators = document.getElementsByClassName('product-indicator'),
+    i = 0;
 
-    $('.product-indicator').on('click', function(e){
-        var id = $(e.currentTarget).data('uuid');
-        $('.product-indicator-active').removeClass('product-indicator-active');
-        $(e.currentTarget).addClass('product-indicator-active');
-        $('.product.product-active').removeClass('product-active');
-        $(".product[data-uuid='"+id+"']").addClass('product-active');
-    });
-});
+product.classList.add('product-active');
+indicator.classList.add('product-indicator-active');
+
+function change(e) {
+    "use strict";
+    var id = e.currentTarget.attributes[2].value;
+    document.querySelector('.product-indicator-active').classList.remove('product-indicator-active');
+    e.currentTarget.classList.add('product-indicator-active');
+    document.querySelector('.product-active').classList.remove('product-active');
+    document.querySelector('.product[data-uuid="' + id + '"]').classList.add('product-active');
+}
+
+for (i; i < indicators.length; i += 1) {
+    indicators[i].addEventListener('click', change, false);
+}
