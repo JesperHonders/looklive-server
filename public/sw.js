198afa755e32190e54def58fc802795a6ecfe078
JesperHonders <jesper.honders@hva.nl>
2016-03-18 11:38:49 +0100

    Fixed SW so it behaves properly

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 this.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('sw-intro-5.1').then(function(cache) {
+        caches.open('app-looklive').then(function(cache) {
             return cache.addAll([
                 './',
                 './js/app.js',
@@ -9,7 +9,34 @@ this.addEventListener('install', function(event) {
 			
         })
     );
-	console.log('Cached')
 });
 
-console.log('test')
+
+//this.addEventListener('fetch', function(event) {
+//    event.respondWith(
+//        caches.match(event.request)
+//            .then(function(response) {
+//                if(response) {
+//                    console.log('found cached response', response);
+//                    return response;
+//                } else {
+//                    console.log('response not in cache, fetching it');
+//                    return fetch(event.request);
+//                }
+//            })
+////    );
+//});
+
+function fetchAndCache(event) {
+
+	return fetch(event.request).then(response) {
+
+		return caches.open('app-looklive').then(function(cache) {
+
+			cache.put(event.request, response.clone());
+			return response;
+
+		});
+
+	}
+}
